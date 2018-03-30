05ebeb1887f950a26ae8c1eeae9a25bddbdc740b
DaRealMechavelli <37198315+DaRealMechavelli@users.noreply.github.com>
2018-03-29 20:52:10 -0400

    Add files via upload

---
+++ code/game_levels.js
@@ -0,0 +1,29 @@
+var GAME_LEVELS = [
+  ["                                                                                                                      ",
+   "                                                                                                                      ",
+   "                                                                                                                      ",
+   "                                                                                                                      ",
+   "                                             r   r   r                                                     r          ",
+   "                                          xxxxxxxxxxxxxx                                          xxxxxxx             ",
+   "                         xxx                                                                               r          ",
+   "                                  xx                                                         xx                       ",
+   "                  xx                                         xxx                  xxxxx                    r          ",
+   "                                                                         r                                            ",
+   "                                                r                      xxxxx                               r          ",
+   "                         o                     xxx                                                                    ",
+   "                        xxx                                                                                           ",
+   "                                       o                                                                              ",
+   "                                      xxx                                                                             ",
+   "                                                                                                                      ",
+   " xx                           xxx                                    o                                             xx ",
+   " x                                                                 xxxxx                                            x ",
+   " x                      xxx                                    xxxxx   xxxxx                                        x ",
+   " x                                                         xxxxx           xxxxx                             o      x ",
+   " x   @                              o  o                             o                                              x ",
+   " xxxxxxxxxxxxxxxxxxxxxxxx                                                                             xxxxxxxxxxxxxxx ",
+   "                        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                ",
+   "                                                                                                                      "],
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
